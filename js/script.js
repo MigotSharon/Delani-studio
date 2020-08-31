e4afec24febf73143dc047f3168814f6e37e8b58
SharonMigot <migotsharon@gmail.com>
2020-08-31 15:20:20 +0300

    added hover

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 $(document).ready(function () {
-    $(document).ready(){
-        $("button").click(function () {
-            $("#showDesign").toggle();
-        });
+    $("#image-1").click(function () {
+        $("#showDesign").toggle();
     });
-};
+});
+
+$(document).ready(function () {
+    $("#image-2").click(function () {
+        $("#showDevelopment").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#image-3").click(function () {
+        $("#showProduct").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#pic1").mouseover(function () {
+        $(".text1").show();
+    }).mouseout(function () {
+        $(".text1").hide();
+    });
+});
+
+
+
+
+
