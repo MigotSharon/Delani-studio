807c8dbcd6dc992eea9f3f0542207b2e0d70b35c
SharonMigot <migotsharon@gmail.com>
2020-08-31 15:33:25 +0300

    added function

--- js/script.js
+++ js/script.js
@@ -24,6 +24,66 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $("#pic2").mouseover(function () {
+        $(".text2").show();
+    }).mouseout(function () {
+        $(".text2").hide();
+    });
+});
+
+$(document).ready(function () {
+    $("#pic3").mouseover(function () {
+        $(".text3").show();
+    }).mouseout(function () {
+        $(".text3").hide();
+    });
+});
+
+
+$(document).ready(function () {
+    $("#pic4").mouseover(function () {
+        $(".text4").show();
+    }).mouseout(function () {
+        $(".text4").hide();
+    });
+});
+$(document).ready(function () {
+    $("#pic5").mouseover(function () {
+        $(".text5").show();
+    }).mouseout(function () {
+        $(".text5").hide();
+    });
+});$(document).ready(function () {
+    $("#pic6").mouseover(function () {
+        $(".text6").show();
+    }).mouseout(function () {
+        $(".text6").hide();
+    });
+});
+
+$(document).ready(function () {
+    $("#pic7").mouseover(function () {
+        $(".text7").show();
+    }).mouseout(function () {
+        $(".text7").hide();
+    });
+});
+
+$(document).ready(function () {
+    $("#pic8").mouseover(function () {
+        $(".text8").show();
+    }).mouseout(function () {
+        $(".text8").hide();
+    });
+});
+
+
+
+
+
+
+
 
 
 
